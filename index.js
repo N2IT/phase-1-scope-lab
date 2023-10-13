fe0c041057febf9f19a0403e321b5375224b486f
Tony Eder <tonyeder11@gmail.com>
2023-10-13 14:40:12 -0700

    passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+setBestCustomer = () => bestCustomer = "not bob";
+
+overwriteBestCustomer = () => bestCustomer = "maybe bob";
+
+const leastFavoriteCustomer = "danny boy";
+
+changeLeastFavoriteCustomer = () => leastFavoriteCustomer = "bob";
